398491684566f8eeb50943674a51afe09dea5d1c
chesanovskiy_dv <chesanovskiydv@gmail.com>
2019-12-03 18:40:07 +0200

    feat(base): add basic template structure

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import Header from "components/Header";
+import Body from "components/Body";
+import {Button} from "@blueprintjs/core";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			title: process.env.REACT_APP_WEBSITE_NAME,
+			navbarFixedToTop: true
+		};
+	}
 
-export default App;
+	handleOpenSettings = () => {
+		// @todo: implement settings dialog
+	};
+
+	render() {
+		const {title, navbarFixedToTop} = this.state;
+		// @todo: add sidebar content
+		const sidebar = (
+			<React.Fragment>
+				Lorem ipsum
+			</React.Fragment>
+		);
+
+		return (
+			<React.Fragment>
+				<Header title={title} fixedToTop={navbarFixedToTop}>
+					<Button className="bp3-minimal" icon="cog" onClick={this.handleOpenSettings}/>
+				</Header>
+
+				<Body offsetTop={navbarFixedToTop} sidebarWidth={230} sidebar={sidebar}>
+					{/* @todo: add calendar */}
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid cumque deleniti, dicta dolorem eius error
+					hic itaque nam necessitatibus nobis quae quasi reprehenderit similique, soluta tempora temporibus tenetur,
+					velit veritatis!
+				</Body>
+			</React.Fragment>
+		)
+	}
+};
---
+++ src/components/Body.js
@@ -0,0 +1,48 @@
+import React from "react";
+import PropTypes from 'prop-types';
+import clsx from "clsx";
+import {Flex, Box} from '@rebass/grid'
+
+import styles from './Body.module.scss';
+
+export default class Header extends React.Component {
+	static propTypes = {
+		children: PropTypes.node,
+		sidebar: PropTypes.node,
+		offsetTop: PropTypes.bool,
+		sidebarWidth: PropTypes.oneOfType([
+			PropTypes.number,
+			PropTypes.string,
+			PropTypes.arrayOf(
+				PropTypes.oneOfType([
+					PropTypes.string,
+					PropTypes.number
+				]),
+			),
+		]),
+	};
+	static defaultProps = {
+		offsetTop: false,
+		sidebarWidth: 1 / 4,
+	};
+
+	render() {
+		const {children, sidebar, offsetTop, sidebarWidth} = this.props;
+
+		return (
+			<Flex className={clsx(styles.body, offsetTop && styles['offset-top'])}>
+				{
+					sidebar && (
+						<Box className={styles.sidebar} width={sidebarWidth}>
+							{sidebar}
+						</Box>
+					)
+				}
+
+				<Box flex='1 1 auto'>
+					{children}
+				</Box>
+			</Flex>
+		)
+	}
+}
